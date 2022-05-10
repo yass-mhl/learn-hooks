f01db97d92b6ac439ebadca4819df1b0c6248039
Yassin Mehlil <yassin.mehlil@gmail.com>
2022-05-10 18:09:20 +0200

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-center'>useState Hooks</h1>
+    </div>
+  );
+}
+
+export default App;
